98d43659d78477ffc33837b610ce945c5d8d1b6f
ruedanivel <ruedanivel1@gmail.com>
2014-02-21 01:03:36 +0100

    dan
    dan

--- js/index.js
+++ js/index.js
@@ -1,79 +1,20 @@
 
+function CargarIMG()
+{	
 
-
-
-var rt= 'primera';
-
-function Inicio()
-{
-
-	if ( rt == 'primera' ){
-
-		$('#IMG > .primera').css("visibility", "hidden");
-		$('#IMG > .a').css("visibility", "hidden");
-		$('#IMG > .b').css("visibility", "hidden");
-
-
-		CargarIMG('primera');
-
-		rt = 'a'
-
-
-	}else if ( rt == 'a'){
-
-		$('#IMG > .primera').css("visibility", "hidden");
-		$('#IMG > .b').css("visibility", "hidden"); 
-
-		$('#IMG > .' + rt).css("visibility", "visible"); // mostrar
-
-		CargarIMG('b');
-
-		rt = 'b'
-
-	
-	}else if ( rt == 'b'){
-
-		$('#IMG > .primera').css("visibility", "hidden"); 
-		$('#IMG > .a').css("visibility", "hidden");
-
-		$('#IMG > .' + rt).css("visibility", "visible"); // mostrar
-
-		CargarIMG('a');
-
-		rt = 'a'
-
-
-		
-	}
-
-
-}
-
-function CargarIMG(rr)
-{
-
-	$('#load > .load').attr('src','images/cargando.gif'); // Circulo
+	alert('1');
 
 	$.ajax({
-		url:'http://regalopeliculas.16mb.com/app/lanzaGato.php',
+		url:'http://musicamaestra.com/programin/app/frasesamor/php.php',
 		type:'post',
 		success:  function (response) {
 		
-			$('#IMG > .' + rr ).attr('src',response);
+				$('#text').html(response);
  
 		}
 	});
 
-}
-
-function ImgOnload(rr)
-{
-
-	$('.load').css("visibility", "hidden");
-	$('#IMG > .primera').css("visibility", "visible");
-
-	CargarIMG('a');
+	alert('2');
 
 }
-
 
