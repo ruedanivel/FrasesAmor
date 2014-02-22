d81f21ab1eadc220fad93354f1f7907a160d7ba2
ruedanivel <ruedanivel1@gmail.com>
2014-02-22 01:25:25 +0100

    dan
    dan

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@
 function CargarIMG()
 {	
 
+	var v_random = Math.floor((Math.random()*5)+1);
+
+	//$('#bg').attr("src", "images/f"+v_random+".jpg");
+
+	$('body').css('background-image','url(images/f'+v_random+'.png)');
+
+
 	$.ajax({
 		url:'http://musicamaestra.com/programin/app/frasesamor/php.php',
 		type:'post',
